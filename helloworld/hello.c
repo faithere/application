e3a5240ee15ace81ef1f6b3fa53a026e09e5d2ef
ubuntu12.04.5 <wzl@ubuntu.(none)>
2016-03-03 00:40:30 -0800

    modify hello.c for strcpy test

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-	printf("hello world!\n");
-	return 0;
+  char p[6];
+  char *test="hello";
+  strcpy(p,test);
+  puts(p);
+ // putchar('\n');
+  printf("%s\n",p);
+  printf("strlen of p=%d  sizeof of p=%d sizof of test=%d strlen of test=%d\n",strlen(p),sizeof(p),sizeof(test),strlen(test));
+  return 0;
 }
