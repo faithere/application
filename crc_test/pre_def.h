b7a5f074a6a2382e27bbcaa1a20496a4f6df3578
wzl <wzl@localhost.localdomain>
2016-05-30 16:33:58 +0800

    Add CRC

---
+++ crc_test/pre_def.h
@@ -0,0 +1,12 @@
+#ifndef _PRE_DEF_H_
+#define _PRE_DEF_H_
+
+typedef char    BOOL;
+typedef unsigned char UCHAR;
+typedef char    CHAR;
+typedef unsigned short USHORT;
+typedef short   SHORT;
+
+typedef unsigned long ULONG;
+typedef long    LONG;
+#endif
