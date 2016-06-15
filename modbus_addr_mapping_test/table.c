0bcae917542b1ba75faa63f1f1c7bc5a1f29014e
wzl <wzl@localhost.localdomain>
2016-06-15 08:53:33 +0800

    Add modbus_table

---
+++ modbus_addr_mapping_test/table.c
@@ -0,0 +1,7 @@
+#include "table.h"
+
+const mem2reg_data level1_reg_table[]={
+  {0,0,ONEBYTE,1,1},
+  {0,1,ONEBYTE,1,2},
+  {1,2,TWOBYTE,1,3},
+  {0,1,ONEBYTE,1,2},
--- modbus_addr_mapping_test/table.h
+++ modbus_addr_mapping_test/table.h
@@ -7,9 +7,15 @@ typedef enum FIELDTYPE
   {
     ONEBYTE=0,
     TWOBYTE=1,
-    FOURBYTE=2
+    FOURBYTE=2,
+    RESERVER=3   //ONLY ONE BYTE
   }field_type_t;
 
+typedef enum{
+  CAT_ONE=0,
+  CAT_TWO=1,
+  CAT_THREE=2
+}category_t;
 
 typedef struct MEM2REG_DATA
 {
@@ -17,11 +23,6 @@ typedef struct MEM2REG_DATA
   uint16_t byteoffset;
   field_type_t field_type;
   uint16_t field_type_num;
-  
-}
-
-
-
-
-
+  uint8_t attrID;
+}mem2reg_data;
 #endif
